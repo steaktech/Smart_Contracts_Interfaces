aa353c7c3c99fc5c2041c7e7fb4f1b026de60164
steaktech (SteakHouse.Finance) <steakhouse_fi@proton.me>
2026-01-04 16:35:49 +0000

    Update IKitchenCreatorBasicAdvanced.sol

--- IKitchenCreatorBasicAdvanced.sol
+++ IKitchenCreatorBasicAdvanced.sol
@@ -3,52 +3,6 @@ pragma solidity ^0.8.20;
 
 import "../KitchenStorage.sol";
 
-interface IKitchenCreatorBasicAdvanced {
-    struct BasicParamsBasic {
-        string name;
-        string symbol;
-        uint256 totalSupply;
-        KitchenStorage.TokenType tokenType;
-        uint256 graduationCap;
-        uint256 lpLockDuration;
-        bool burnLP;
-        uint256 startTime;
-        uint256 finalTaxRate;
-        bool removeHeader;
-    }
-
-    struct BasicParamsAdvanced {
-        string name;
-        string symbol;
-        uint256 totalSupply;
-        KitchenStorage.TokenType tokenType;
-        uint256 graduationCap;
-        uint256 lpLockDuration;
-        bool burnLP;
-        uint256 startTime;
-        uint256 finalTaxRate;
-        bool removeHeader;
-    }
-
-    struct StaticCurveParams {
-        uint256 curveStartingTax;
-        uint256 curveTaxDuration;
-        uint256 curveMaxWallet;
-        uint256 curveMaxWalletDuration;
-        uint256 curveMaxTx;
-        uint256 curveMaxTxDuration;
-    }
-
-    struct AdvancedParamsInput {
-        uint256 taxDropStep;
-        uint256 taxDropInterval;
-        uint256 maxWalletStep;
-        uint256 maxWalletInterval;
-        uint256 maxTxStep;
-        uint256 maxTxInterval;
-        uint256 limitRemovalTime;
-    }
-
     function createBasicToken(BasicParamsBasic calldata b, StaticCurveParams calldata s, address creator)
         external
         payable;
